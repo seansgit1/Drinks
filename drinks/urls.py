e434325cc40dfef0bc101ac20afd8f04e26b359a
seansgit <simon.small141@gmail.com>
2024-05-01 19:41:16 +0200

    First commit

---
+++ drinks/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from drinks import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('drinks/', views.drink_list),
+    path('drinks/<int:id>', views.drink_detail)
+]
